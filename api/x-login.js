7e0d89a7b86d9fa0165624fa70e80fb70846edc2
Prathamesh Jadhav <prathameshj17@gmail.com>
2026-08-09 23:18:10 -0700

    Add media upload permission

--- api/x-login.js
+++ api/x-login.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
     response_type: 'code',
     client_id: clientId,
     redirect_uri: redirectUri,
-    scope: 'tweet.read tweet.write users.read offline.access',
+    scope: 'tweet.read tweet.write users.read media.write offline.access',
     state,
     code_challenge: codeChallenge,
     code_challenge_method: 'S256'
